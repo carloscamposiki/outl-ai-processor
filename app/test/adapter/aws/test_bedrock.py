81c3d8b688be09ff85a981b79a7e2939dedf0a26
Carlos Campos <carloscamposiki@gmail.com>
2025-07-31 21:56:01 -0300

    fix: bedrock invoke

--- app/test/adapter/aws/test_bedrock.py
+++ app/test/adapter/aws/test_bedrock.py
@@ -18,7 +18,7 @@ class TestBedrockAdapter(unittest.TestCase):
         model_id = "test-model"
         input_text = "test input"
         expected_body = {
-            'prompt': f'\n\nHuman:\n\n{input_text}',
+            'prompt': f'\n\nHuman:\n\n{input_text}\n\nAssistant:\n\n',
             'max_tokens_to_sample': 2000
         }
 
@@ -44,7 +44,7 @@ class TestBedrockAdapter(unittest.TestCase):
         model_id = "test-model"
         input_text = "test input"
         expected_body = {
-            'prompt': f'\n\nHuman:\n\n{input_text}',
+            'prompt': f'\n\nHuman:\n\n{input_text}\n\nAssistant:\n\n',
             'max_tokens_to_sample': 2000
         }
 
